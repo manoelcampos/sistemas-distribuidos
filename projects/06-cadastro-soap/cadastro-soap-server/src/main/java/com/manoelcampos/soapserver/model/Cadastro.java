39c8f47936506ea84b4a872f3ffe1d2811aa9b50
Manoel Campos <manoelcampos@gmail.com>
2018-11-06 00:25:41 -0300

    Atualiza documentação do projeto soap

--- projects/06-cadastro-soap/cadastro-soap-server/src/main/java/com/manoelcampos/soapserver/model/Cadastro.java
+++ projects/06-cadastro-soap/cadastro-soap-server/src/main/java/com/manoelcampos/soapserver/model/Cadastro.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 
 
 /**
- *
+ * Interface a ser utilizada por classe de negócio dentro
+ * deste mesmo pacote, que representam as Entities da JPA,
+ * ou seja, as tabelas do banco de dados.
+ * 
  * @author Manoel Campos da Silva Filho
  */
 public interface Cadastro extends Serializable {
