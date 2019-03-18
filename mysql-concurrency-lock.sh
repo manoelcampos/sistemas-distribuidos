07170b843ca4239522d9de698bb866809913d09b
Manoel Campos <manoelcampos@gmail.com>
2019-03-17 21:45:54 -0300

    Atualiza ebook

---
+++ mysql-concurrency-lock.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+clear
+
+SENHA_ROOT="root"
+
+echo "Mostra como é possível usar o comando 'select ... for update' do MySQL para criar um lock"
+echo "e assim impedir que outro usuário (em outra transação) consiga fazer alterações"
+echo "enquanto as alterações do primeiro usuário não forem confirmadas com o comando commit"
+echo ""
+echo "Para testar este exemplo, abra dois terminais e execute o script em cada um."
+echo "Você verá que o segundo fica parado a espera do update no primeiro ser confirmado (commit)."
+echo ""
+echo "CERTIFIQUE-SE DE INSERIR A SENHA DO USUÁRIO ROOT DO MYSQL DENTRO DESTE SCRIPT."
+echo ""
+echo ""
+
+#Cria um arquivo SQL na pasta temporária, contendo os comandos a serem executados no banco
+echo "create database if not exists concorrencia;
+use concorrencia;
+
+# Cria a tabela se ela não existir e insere um registro nela.
+create table if not exists cidade as select 1 as id, 'Palmas                                            ' as nome;
+
+select '    Tentando selecionar dados (pode demorar por conta de lock em outra operação paralela)' as Mensagem;
+
+# Locks só funcionam dentro de transações
+start transaction;
+
+# Seleciona o registro da cidade 1, criando um lock para evitar o acesso
+# enquanto a transação não for finalizada.
+# Como vamos alterar o registro logo abaixo, o lock impede o acesso ao registro 
+# neste meio tempo.
+select * from cidade where id = 1 for update;
+
+# Altera o nome da cidade para um valor aleatório
+update cidade set nome = concat('Aleatório ', rand()) where id = 1;
+
+select '    Aguardando alguns segundos para conseguirmos perceber o lock' as Mensagem;
+
+# Aguarda alguns seguindos. Neste intervalo você pode executar o script em outro terminal
+# e verá que ele não consegue nem sequer obter os dados do registro com o primeiro select acima.
+select sleep(10);
+
+# Confirma as alterações e libera o lock.
+# Se foi o script foi aberto em outro terminal,
+# a partir deste momento a outra instância do script terá acesso
+# ao registro que estava bloqueado.
+commit;
+
+# Exibe os dados depois da confirmação das alterações acima.
+# Se colocarmos um sleep antes deste select, poderemos inclusive
+# ver alterações feitas por outros usuários (outras instâncias deste script).
+select * from cidade;
+" > /tmp/script.sql
+
+# Executa os comandos do arquivo SQL criado acima
+mysql -u root -p$SENHA_ROOT < /tmp/script.sql
--- projects/MapReduceApp.java
+++ projects/MapReduceApp.java
@@ -1,21 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Mostra como é extremamente simples implementar o modelo de programação
+ * <a href="https://pt.wikipedia.org/wiki/MapReduce">MapReduce</a> 
+ * utilizando recursos de <a href="https://www.oracle.com/technetwork/pt/articles/java/streams-api-java-8-3410098-ptb.html">Streams e Expressões Lambda do Java 8</a>
+ * para processar dados em paralelo (utilizando múltiplas CPUs).
+ * 
+ * <p>Diferentemente de ferramentas como o Apache Hadoop, utilizadas
+ * para processamento distribuído de dados (em vários computadores),
+ * Streams do Java 8 só permitem dividir o processamento de dados
+ * entre as diferentes CPUs de uma mesma máquina.
+ * </p>
+ * 
+ * @author Manoel Campos da Silva Filho
+ */
 public class MapReduceApp{
     private List<Pessoa> pessoas;
 
+    public static void main(String args[]){
+        MapReduceApp app = new MapReduceApp();
+        System.out.println("Total de mulheres: " + app.contaPessoas('F'));
+    }
+
     private MapReduceApp(){
         pessoas = new ArrayList<>(10);
-        pessoas.add(new Pessoa("Manoel", 'M'));
-        pessoas.add(new Pessoa("João", 'M'));
-        pessoas.add(new Pessoa("Andreia", 'F'));
-        pessoas.add(new Pessoa("Pedro", 'M'));
-        pessoas.add(new Pessoa("Maria", 'F'));
-        pessoas.add(new Pessoa("Joana", 'F'));
-        pessoas.add(new Pessoa("Bete", 'F'));
-        pessoas.add(new Pessoa("Márcio", 'M'));
-        pessoas.add(new Pessoa("Luiza", 'F'));
-        pessoas.add(new Pessoa("André", 'M'));
+        pessoas.add(new Pessoa("Manoel",  'M'));
+        pessoas.add(new Pessoa("João",    'M'));
+        pessoas.add(new Pessoa("Andréia", 'F'));
+        pessoas.add(new Pessoa("Pedro",   'M'));
+        pessoas.add(new Pessoa("Sara",    'F'));
+        pessoas.add(new Pessoa("Tereza",  'F'));
+        pessoas.add(new Pessoa("Bete",    'F'));
+        pessoas.add(new Pessoa("Roberto", 'M'));
+        pessoas.add(new Pessoa("Luiza",   'F'));
+        pessoas.add(new Pessoa("Cláudia", 'F'));
     }
 
     /**
@@ -28,16 +47,31 @@ public class MapReduceApp{
      * que só é compensada se tivermos processamento muitos dados e/ou
      * executando operações complexas sobre tais dados.
      *
-     * @param sexo sexo para buscar o total de pessoas
+     * @param sexo sexo das pessoas que deseja contar
      * @return o total de pessoas do sexo indicado
      */
     public long contaPessoas(char sexo){
+        /* 
+           A linha abaixo, apesar de conter várias chamadas de métodos, é uma única instrução
+           encadeada (após a chamada de uma função, colocamos ponto para chamar outra).
+           Veja que há apenas um ponto-e-vírgula.
+           Indo de ponto em ponto, temos:
+           - pessoas: obtém a lista de pessoas.
+           - stream(): acessa tal lista como um objeto Stream do Java 8 para permitir processamento dos dados.
+           - parallel(): permitir que seja feita a divisão automática dos dados da lista para processamento 
+                         em várias CPUs. Se removido, executará tudo em uma única CPU.
+           - filter(): filtra a lista de pessoas. Para isto, estamos utilizando o recurso
+                       de Expressões Lambda do Java 8 (identificado pelo uso da seta ->)
+                       para fornecer uma função anônima que define como as pessoas da lista
+                       serão filtradas. Neste caso, pessoa é uma variável que implicitamente
+                       será do tipo Pessoa (o tipo dos elementos da lista pessoas).
+                       Depois da seta temos o código da nossa função anônima (neste caso uma única
+                       linha), que define que queremos obter apenas as pessoas do sexo indicado 
+                       no parâmetro sexo
+            - count(): esta é uma operação de redução, que vai contar o total de pessoas do sexo
+                       indicado e retornar tal número.
+        */
         return pessoas.stream().parallel().filter(pessoa -> pessoa.getSexo() == sexo).count();
-    }
-
-    public static void main(String args[]){
-        MapReduceApp app = new MapReduceApp();
-        System.out.println("Total de mulheres: " + app.contaPessoas('F'));
     }
 }
 
