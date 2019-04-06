6c807992521149219d111e41b7eb5f3a4e80ba03
Manoel Campos <manoelcampos@gmail.com>
2019-04-06 14:24:59 -0300

    Atualiza projeto programação funcional

--- projects/00-programacao-funcional/src/main/java/AppProgramacaoFuncional.java
+++ projects/00-programacao-funcional/src/main/java/AppProgramacaoFuncional.java
@@ -1,14 +1,17 @@
 import java.util.List;
 
 /**
- * Aplicação de exemplo de princípios de programação
- * funcional em Java, Expressões Lambda e API de Streams do Java 8.
+ * Aplicação de exemplo de princípios de programação funcional em Java,
+ * Expressões Lambda e API de Streams do Java 8.
  *
  * Para aprofundar nestes assuntos, veja os links abaixo:
  *
  * <ul>
- *     <li><a href="https://www.oracle.com/technetwork/pt/articles/java/streams-api-java-8-3410098-ptb.html">Curso Streams e Expressões Lambda do Java 8</a></li>
- *     <li><a href="https://apexapps.oracle.com/pls/apex/f?p=44785:145:0::::P145_EVENT_ID,P145_PREV_PAGE:4887,143">Curso JDK 8 MOOC: Lambdas and Streams Introduction</a></li>
+ * <li><a href=
+ * "https://www.oracle.com/technetwork/pt/articles/java/streams-api-java-8-3410098-ptb.html">Curso
+ * Streams e Expressões Lambda do Java 8</a></li>
+ * <li><a href= "http://bit.ly/2I2U5bU">Curso JDK 8 MOOC: Lambdas and Streams
+ * Introduction</a></li>
  * </ul>
  *
  * @author Manoel Campos da Silva Filho
@@ -24,10 +27,33 @@ public class AppProgramacaoFuncional {
     public static void main(String[] args) {
         AppProgramacaoFuncional app = new AppProgramacaoFuncional();
         app.printStudents();
+        System.out.println(app.maxScore());
     }
 
     private void printStudents(){
+        for (Student student : students) {
+            System.out.println(student);
+        }
     }
 
+    private double maxScore(){
+        return students.stream()
+                .filter(student -> filterStudents(student))
+                .mapToDouble(student -> student.getScore())
+                .max()
+                .orElse(0.0);
+    }
+
+    private double averageScore(){
+        return students.stream()
+                .filter(student -> filterStudents(student))
+                .mapToDouble(student -> student.getScore())
+                .average()
+                .orElse(0.0);
+    }
+
+    private boolean filterStudents(Student student) {
+        return student.getGradYear() == 2011;
+    }
 }
 
