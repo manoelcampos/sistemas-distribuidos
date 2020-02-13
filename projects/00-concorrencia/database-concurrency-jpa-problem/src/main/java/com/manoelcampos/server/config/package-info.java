1e79b6cc9429b1eccc7ef0e307e3175b255f3d29
Manoel Campos <manoelcampos@gmail.com>
2020-02-13 14:59:12 -0300

    Remove RestConfig class e usa variável quarkus.resteasy.path no lugar

--- projects/00-concorrencia/database-concurrency-jpa-problem/src/main/java/com/manoelcampos/server/config/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contém classes que realizam algumas configurações do projeto,
- * como configuração dos Web Services e criação de objetos
- * complexos como EntityManager e DAOs.
- * 
- * @author Manoel Campos da Silva Filho
- */
-package com.manoelcampos.soapserver.ws;
