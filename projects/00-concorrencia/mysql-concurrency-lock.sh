a1d64062cd3ee795fff41f04a4d82707b708bcd4
Manoel Campos <manoelcampos@gmail.com>
2020-10-23 09:09:53 -0300

    Atualia script concorrência mysql

--- projects/00-concorrencia/mysql-concurrency-lock.sh
+++ projects/00-concorrencia/mysql-concurrency-lock.sh
@@ -12,7 +12,8 @@ echo "e assim impedir que outro usuário (em outra transação) consiga fazer al
 echo "em um registro enquanto as alterações do primeiro usuário não forem confirmadas com o comando commit"
 echo ""
 echo "Para testar este exemplo, abra dois terminais e execute o script em cada um."
-echo "Você verá que o segundo fica parado a espera do update no primeiro ser confirmado (commit)."
+echo "Cada terminal representa um usuário acessando o banco."
+echo "Você verá que o segundo fica parado à espera do update no primeiro ser confirmado (commit)."
 echo ""
 echo "CERTIFIQUE-SE DE INSERIR A SENHA DO USUÁRIO ROOT DO MYSQL DENTRO DESTE SCRIPT."
 echo ""
@@ -25,7 +26,7 @@ use concorrencia;
 # Cria a tabela se ela não existir e insere um registro nela.
 create table if not exists cidade as select 1 as id, 'Palmas                                            ' as nome;
 
-select '# Tentando selecionar dados (pode demorar por conta de lock em outra operação paralela)' as '';
+select '# Tentando selecionar dados (pode demorar por conta de lock em outra operação concorrente)' as '';
 
 # Locks só funcionam dentro de transações
 start transaction;
@@ -42,7 +43,9 @@ select * from cidade where id = 1 for update;
 \! echo '# e verá que ele não consegue nem sequer obter os dados do registro usando o primeiro select.'
 \! echo ''
 
-# Altera o nome da cidade para um valor aleatório (aguardando 10 segundos para concluir o update)
+# Altera o nome da cidade para um valor aleatório.
+# Aguarda 10 segundos de propósito, para simular uma demora na conclusão do update.
+# Assim, você consegue ver que o segundo terminal fica parado esperando este update finalizar.
 update cidade set nome = concat('Aleatório ', rand()) where id = 1 and sleep(10) >= 0;
 
 # Confirma as alterações e libera o lock.
